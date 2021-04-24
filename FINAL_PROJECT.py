3b1e050a063f7878d32951e072135c3fd1d6fee5
orladoyle3 <orladoyle86@gmail.com>
2021-04-24 16:22:26 +0100

    Second Commit

---
+++ FINAL_PROJECT.py
@@ -0,0 +1,12 @@
+'#import required libraries'
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+
+os.getcwd()
+print(os.getcwd())
+
+data = pd.read_csv("C:/Users/orlad/OneDrive/Desktop/OD_PROJECT.gdp_csv.csv","r")
+
