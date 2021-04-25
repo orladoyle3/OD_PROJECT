10dbf32d1ce700bce12aba52f516a9bb3bed01ca
orladoyle3 <orladoyle86@gmail.com>
2021-04-25 22:48:40 +0100

    Fourth commit - clean data

--- FINAL_PROJECT.py
+++ FINAL_PROJECT.py
@@ -4,8 +4,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+'#import dataframes'
 gdp= pd.read_csv('gdp_csv.csv')
-print(gdp.info())
 
 eu = pd.read_csv('states.csv')
-print(eu.info())
+
+'#change column name in eu dataframe from Country to Country Name'
+eu_edit = eu.rename(columns={'Country':'Country Name'})
+
+'#merge the dataframes on column Country Name using a left join'
+gdp_eu = pd.merge(gdp, eu_edit, on='Country Name', how='left')
+
+'#drop columns not required'
+col_list = ['Council Votes', 'European Parliament Seats', 'European Free Trade Agreement', 'European Single Market', 'European Monetary Union', 'Currency', 'Currency Code', 'Language','GDP ($, millions)','GDP per capita ($, millions)']
+gdp_eu = gdp_eu.drop(col_list, axis=1)
+
+'#replace missing values in dataset'
+gdp_eu = gdp_eu.fillna(0)
+
+'# group data by European Union - Member'
+gdp_eu1 = gdp_eu.groupby('European Union')['GDP (€, millions)'].plot(legend=True)
+
+gdp_eu['Per Capita'] = gdp_eu['GDP (€, millions)'] / gdp_eu['Population']
+
