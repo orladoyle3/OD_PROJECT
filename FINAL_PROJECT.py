008a5f4ac81be22caf8a95853385d7d781928ea3
orladoyle3 <orladoyle86@gmail.com>
2021-04-28 13:01:35 +0100

    Seventh commit - First Chart finalised

--- FINAL_PROJECT.py
+++ FINAL_PROJECT.py
@@ -28,28 +28,26 @@ gdp_eu = gdp_eu[(gdp_eu['European Union'] !=0) | ((gdp_eu["Country Code"] == "US
 '#calculate gdp per head of population for each EU country'
 us_pop = 323100000
 gdp_eu["Population"] = gdp_eu["Population"].replace(0,323100000)
-gdp_eu["Per Capita"] = round((gdp_eu['Value'] / gdp_eu['Population']),2)
+gdp_eu["Per Capita"] = round(((gdp_eu['Value'] / gdp_eu['Population']))/1e3,0)
 
 '# create two datasets for the years 2006 and 2016'
 gdp06 = gdp_eu[(gdp_eu["Year"] == 2006)]
-gdp06 = gdp06.sort_values(["Per Capita", "Country Name"], ascending=(True, False))
+gdp06 = gdp06.sort_values(["Per Capita"], ascending=(True))
 gdp16 = gdp_eu[(gdp_eu["Year"] == 2016)]
-gdp16 = gdp16.sort_values(["Per Capita", "Country Name"], ascending=(True,False))
+gdp16 = gdp16.sort_values(["Per Capita"], ascending=(True))
 
 '#Plot 2010 and 2016 per Capita to show increase or decrease in value over a 10 year period'
 fig, ax=plt.subplots()
-ax.plot(gdp06["Country Name"], gdp06["Per Capita"], marker = "o", linewidth=0.5)
-ax.plot(gdp16["Country Name"], gdp16["Per Capita"], marker = "o", linewidth=0.5)
+ax.plot(gdp06["Country Name"], gdp06["Per Capita"], marker = "o", linestyle="None")
+ax.plot(gdp16["Country Name"], gdp16["Per Capita"], marker = "o", linestyle ="None")
 ax.set_xticklabels(gdp06["Country Name"], rotation=80)
-ax.set_yticklabels(gdp06["Per Capita"])
-ax.set(title="2010 GDP vs 2016 GDP Per Country", xlabel="Country", ylabel="Per Capita", ylim=[0,50000])
+ax.set_yticklabels(gdp16["Per Capita"])
+ax.set(title="2010 GDP vs 2016 GDP Per Capita", xlabel="Country", ylabel="Per Capita ('000's)")
+ax.set_ylim([0, gdp16["Per Capita"].max()])
 ax.margins(x=0, y=0.1)
 fig.tight_layout()
 ax.legend([2006, 2016])
 plt.show()
 
-
-gdp_eu1 = gdp_eu.groupby('European Union')['Value'].sum() /1e9
-gdp_eu1.to_csv('edit20.csv')
-gdp_eu1.hist()
-plt.show()
+'# loop through dataframe to identify IRL and USA data'
+for i in "Country Code"
